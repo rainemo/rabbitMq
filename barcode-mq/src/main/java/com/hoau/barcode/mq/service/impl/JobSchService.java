5c5c9c528f9190c7ddabd3317b90ea7f1eb31610
张超 <zhangchao10@itiaoling.com>
2017-09-12 17:01:22 +0800

    LOGBACK应用     实现 将日志存放到文件夹、输出到控制台。按时间大小、分割日志

--- barcode-mq/src/main/java/com/hoau/barcode/mq/service/impl/JobSchService.java
+++ barcode-mq/src/main/java/com/hoau/barcode/mq/service/impl/JobSchService.java
@@ -3,6 +3,8 @@ package com.hoau.barcode.mq.service.impl;
 import com.hoau.barcode.mq.dao.JobSchDao;
 import com.hoau.barcode.mq.entity.JobSchEntity;
 import com.hoau.barcode.mq.service.IJobSchService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,8 +15,13 @@ public class JobSchService implements IJobSchService {
 
     @Autowired
     private JobSchDao jobSchDao;
-
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     public List<JobSchEntity> query() {
+        log.trace("trace=======>trace");
+        log.debug("debug=======>debug");
+        log.info("info=======>info");
+        log.warn("warn=======>warn");
+        log.error("error=======>error");
         return jobSchDao.query();
     }
 }
